559387a36c7e08a37a98f771a0fea01e0e9cb3e4
Luan Nijou <126830016+Luan-Nijou@users.noreply.github.com>
2023-06-07 15:50:29 -0300

    Update Code.ino

--- Code.ino
+++ Code.ino
@@ -7,12 +7,12 @@ int LDR_PIN = A0;
 int Temp_PIN = A1;
 int Hum_Pin = A2;
 int Quant_Liquido_Pin = A3;
+const int BUZZER_PIN = 9;
 
 int luminosity;
-float tempC = 0;
-int sensorhumidade = 0;
-float Armazen_Solu_Nutri = 0;
-const int BUZZER_PIN = 9;
+float tempC;
+int sensorhumidade;
+float Armazen_Solu_Nutri;
 
 
 // Variáveis para calcular a temperatura 
@@ -21,18 +21,18 @@ double Voltage = 0;
 
 
 
-// Define os limiares dos niveis do ambiente
-//(luminosidade, umidade, temperatura e reserva)
-int MODO_OK = 870;    // Nível de luminosidade considerado OK
-int MODO_ALERTA = 950;  // Nível de luminosidade considerado em ALERTA
+/* Define os limiares dos niveis do ambiente
+(luminosidade, umidade, temperatura e reserva)
+*/
+int Lumi_OK = 870;    // Nível de luminosidade considerado OK
+int Lumi_ALERTA = 950;  // Nível de luminosidade considerado em ALERTA
 int humi_OK = 30; // Nível de umidade considerado OK
 int humi_ALERTA = 70; // Nível de umidade considerado ALERTA
 int temp_OK = 15 ;  // Nível de temperatura considerado OK  
 int temp_ALERTA = 25; // Nível de temperatura considerado ALERTA
-int Solu_Nutri_ALERTA = 1000;
+int Solu_Nutri_ALERTA = 1000; // Nível de reserva considerado ALERTA
 
-// Define o estado atual do sistema
-int LumiState = 0;  // 0 = OK, 1 = Alerta, 2 = Problema
+
 
 /*Definindo saída ou entrada de informações, inicio do lcd
 e início do Serial
@@ -41,25 +41,46 @@ void setup() {
   lcd.begin(16, 2);
   lcd.clear();
   pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(LDR_PIN, INPUT);
+  pinMode(Temp_PIN, INPUT);
+  pinMode(Hum_Pin, INPUT);
+  pinMode(Quant_Liquido_Pin, INPUT);
   Serial.begin(9600);
-  pinMode(A2, INPUT);
-  pinMode(A3, INPUT);
-  
-  
-  
+   
 }
  
+
+//Função para controle de luminosidade
+  void Lumi_Rod() {
+    if(luminosity <= Lumi_OK){
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Pouca Luz");
+      noTone(BUZZER_PIN);
+    }else if(luminosity <= Lumi_ALERTA){      	
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Luz OK");
+      noTone(BUZZER_PIN);
+    }else{
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Muita Luz");
+      tone(BUZZER_PIN,1000,3000);
+    }
+  }
+      
+
 //Função para controle da quantidade de um liquido 
 void Solu_Nutri_Rod() {
   if (map(Armazen_Solu_Nutri,0,1023,0,2000) <= Solu_Nutri_ALERTA) {
- 	 lcd.clear();
-  	 lcd.setCursor(0, 0);
-  	 lcd.print("Reserva BAIXA");
-  	 lcd.setCursor(0, 1);
-     lcd.print(map(Armazen_Solu_Nutri,0,1023,0,2000)); 
-     lcd.print("L"); 
-     tone(BUZZER_PIN,1000,3000);
- 	
+ 	lcd.clear();
+  	lcd.setCursor(0, 0);
+  	lcd.print("Reserva BAIXA");
+ 	lcd.setCursor(0, 1);
+    lcd.print(map(Armazen_Solu_Nutri,0,1023,0,2000)); 
+    lcd.print("L"); 
+    tone(BUZZER_PIN,1000,3000); 	
     } else {
     lcd.clear();
   	lcd.setCursor(0, 0);
@@ -71,19 +92,19 @@ void Solu_Nutri_Rod() {
   }
 }
 
+
 //Função para controle de temperatura
 void Temp_Rod() {
   if (tempC<temp_OK) {
- 	 lcd.clear();
-  	 lcd.setCursor(0, 0);
-  	 lcd.print("Temp. Baixa ");
-  	 lcd.setCursor(0, 1);
- 	 lcd.print("Temp. = ");
-  	 lcd.println(tempC);
-     lcd.setCursor(14, 1);
-     lcd.print("C"); 
-     tone(BUZZER_PIN,1000,3000);
-    
+ 	lcd.clear();
+  	lcd.setCursor(0, 0);
+  	lcd.print("Temp. Baixa ");
+  	lcd.setCursor(0, 1);
+    lcd.print("Temp. = ");
+  	lcd.println(tempC);
+    lcd.setCursor(14, 1);
+    lcd.print("C"); 
+    tone(BUZZER_PIN,1000,3000);
     }else if (tempC<temp_ALERTA){
     lcd.clear();
   	lcd.setCursor(0, 0);
@@ -103,25 +124,24 @@ void Temp_Rod() {
     lcd.setCursor(14, 1);
     lcd.print("C"); 
     tone(BUZZER_PIN,1000,3000);
-    
-  
   }
 }
+
 
 /*Função para controle de umidade. Com base em porcentagem, para 
 menor que 30% seja umidade BAIXA; para menor que 70% seja OK;
 e para maior que 70% esteja ALERTA.*/
 void Humi_Rod() {
   if (map(sensorhumidade, 0, 1023, 0, 100)<humi_OK) {
- 	 lcd.clear();
-  	 lcd.setCursor(0, 0);
-  	 lcd.print("Umid. Baixa ");
-  	 lcd.setCursor(0, 1);
- 	 lcd.print("Umid. = ");
-     lcd.setCursor(8, 1);
-  	 lcd.print(map(sensorhumidade, 0, 1023, 0, 100));
-     lcd.println("%");
-     tone(BUZZER_PIN,1000,3000);    
+ 	lcd.clear();
+  	lcd.setCursor(0, 0);
+  	lcd.print("Umid. Baixa ");
+  	lcd.setCursor(0, 1);
+ 	lcd.print("Umid. = ");
+    lcd.setCursor(8, 1);
+  	lcd.print(map(sensorhumidade, 0, 1023, 0, 100));
+    lcd.println("%");
+    tone(BUZZER_PIN,1000,3000);    
     }else if (map(sensorhumidade, 0, 1023, 0, 100)<humi_ALERTA){
     lcd.clear();
   	lcd.setCursor(0, 0);
@@ -144,7 +164,7 @@ void Humi_Rod() {
   }
 }
 
-// Começo para rodar o código
+// Começo que rodará o código
 void loop() {
   luminosity = analogRead(LDR_PIN); // Lê o valor do LDR
   sensorhumidade = analogRead(A2); // Lê o valor simulado da porta de umidade
@@ -162,79 +182,20 @@ void loop() {
   Serial.print("\n Humidade: "); 
   Serial.print(map(sensorhumidade, 0, 1023, 0, 100));
   Serial.println("%");
+  Serial.print("\n Reserva: ");
+  Serial.print(map(Armazen_Solu_Nutri, 0, 1023, 0, 2000));
+  Serial.println("L");
   delay(1000);
     
  
-  //Void luminosidade 
-  if (luminosity <= MODO_OK) {
-    LumiState = 0;  // OK
-    
-  } else if (luminosity <= MODO_ALERTA) {
-    LumiState = 1;  // Alerta
-  } else {
-    LumiState = 2;  // Problema
-  }
+  Humi_Rod();
+  delay(2000);
+  Temp_Rod();
+  delay(2000);
+  Solu_Nutri_Rod();
+  delay(2000);
+  Lumi_Rod();
   
-  
-  /* Verifica o estado atual do sistema. Cada caso, 
-     lê a luminosidade, a temperatura, a umidade e 
-     a reserva do líquido.
-  */
-   switch (LumiState) {
-  	case 0:      
-      noTone(BUZZER_PIN);
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Pouca Luz");
-     delay(1000);
-    while(true){
-        Solu_Nutri_Rod();
-        delay(1000);
-    	Temp_Rod();
-      	delay(1000);
-      	Humi_Rod();
-      	delay(1000);
-      	break;
-    	}
-      break;
-    delay(3000);
-    
-    case 1: 
-      noTone(BUZZER_PIN);      	
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Luz OK");   
-      delay(1000);
-    while(true){
-        Solu_Nutri_Rod();
-        delay(1000);
-    	Temp_Rod();
-      	delay(1000);
-      	Humi_Rod();
-      	delay(1000);
-      	break;
-    	}
-      break;
-    delay(3000);
-    
-    case 2:
-      tone(BUZZER_PIN,1000,3000);
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Muita Luz");
-      delay(1000);
-    while(true){
-        Solu_Nutri_Rod();
-        delay(1000);
-    	Temp_Rod();
-      	delay(1000);
-      	Humi_Rod();
-      	delay(1000);
-      	break;
-    	}
-      break;
-    delay(3000);
-   }
 }
   
 
